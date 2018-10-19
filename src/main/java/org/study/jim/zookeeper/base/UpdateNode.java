133c9865eb9e5ed617b7acdf7656202445dd75e5
1290414968 <1290414968@qq.com>
2018-10-19 09:00:08 +0800

    demo的测试内容的更新

--- src/main/java/org/study/jim/zookeeper/base/UpdateNode.java
+++ src/main/java/org/study/jim/zookeeper/base/UpdateNode.java
@@ -15,15 +15,15 @@ public class UpdateNode implements Watcher {
             semaphore.await();
             String path = "/jim-sync-update";
             //创建
-            String  syncPath =  zooKeeper.create(path,"0".getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+            String  syncPath =  zooKeeper.create(path,"{username:1}".getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
             System.err.println(syncPath);
             //更新
             zooKeeper.getData(path,true,null);
             // -1 参数，zookeeper会根据该值进行当前节点最新版本基础上的更新
-            Stat stat  =  zooKeeper.setData(path,"1".getBytes(),-1);
+            Stat stat  =  zooKeeper.setData(path,"{username:2}".getBytes(),-1);
             System.err.println(stat.getMzxid()+"|"+stat.getVersion());
             // 返回当前节点的最新版本号，然后将版本作为参数进行更新
-            Stat stat2 = zooKeeper.setData(path,"2".getBytes(),stat.getVersion());
+            Stat stat2 = zooKeeper.setData(path,"{username:3}".getBytes(),stat.getVersion());
             // stat版本已过期，更新报错
 //            zooKeeper.setData(path,"2".getBytes(),stat.getVersion());
             System.err.println(stat2.getMzxid()+"|"+stat2.getVersion());
